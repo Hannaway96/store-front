5e09a945beef768198c678153e2721dafd4f9214
Jack Hannaway <jack_hannaway@icloud.com>
2025-11-01 21:02:18 +0000

    Additional Tests for User model

--- app/backend/app/settings.py
+++ app/backend/app/settings.py
@@ -139,3 +139,5 @@ STATIC_ROOT = "/vol/web/static"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = 'core.User'
