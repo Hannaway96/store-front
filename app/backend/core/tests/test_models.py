04aa58ffa33a68fa884c5c126b44c39cddea32e1
Jack Hannaway <jack_hannaway@icloud.com>
2025-11-01 21:46:26 +0000

    Added custom User page on Django Admin site + Tests

--- app/backend/core/tests/test_models.py
+++ app/backend/core/tests/test_models.py
@@ -2,11 +2,12 @@
 Test Models in the system
 """
 
-from unittest.mock import patch
 from decimal import Decimal
-from django.test import TestCase
-from django.contrib.auth import get_user_model
+from unittest.mock import patch
+
 from core import models
+from django.contrib.auth import get_user_model
+from django.test import TestCase
 
 
 def create_user(email="user@example.com", password="testpass123"):
@@ -35,19 +36,18 @@ class ModelTests(TestCase):
         ]
 
         for email, expected in emails:
-            user = get_user_model().objects.create_user(email, 'sample123')
+            user = get_user_model().objects.create_user(email, "sample123")
             self.assertEqual(user.email, expected)
-        
+
     def test_new_user_without_email_raises_error(self):
         """Test creating a new user without an email raises an error"""
         with self.assertRaises(ValueError):
-            get_user_model().objects.create_user('', 'sample123')
-    
-    
+            get_user_model().objects.create_user("", "sample123")
+
     def test_create_super_user(self):
         """Test creating a super user"""
-        user = get_user_model().objects.create_superuser("example@mail.com", "password123")
+        user = get_user_model().objects.create_superuser(
+            "example@mail.com", "password123"
+        )
         self.assertEqual(user.is_staff, True)
         self.assertEqual(user.is_superuser, True)
-
-    
