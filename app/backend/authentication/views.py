32e2126866828bce3db91d3ee3f0925363ce1f56
Jack Hannaway <jack_hannaway@icloud.com>
2025-11-17 12:08:21 +0000

    Made Authentication it's own app and resource group

---
+++ app/backend/authentication/views.py
@@ -0,0 +1,50 @@
+"""
+Views for Authentication
+"""
+
+from rest_framework import status
+from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+
+from .serializers import RegisterRequestSerializer, RegisterResponseSerializer
+
+
+class RegistrationView(CreateAPIView):
+    """
+    API View for Registering new Users
+    POST - /regsiter
+    """
+
+    serializer_class = RegisterRequestSerializer
+    permission_classes = [AllowAny]
+
+    def create(self, request):
+        """
+        create function overrides POST logic
+        """
+        serializer = self.get_serializer(data=request.data)
+        if not serializer.is_valid():
+            # Check if the error is due to an existing user
+            if "email" in serializer.errors:
+                for error in serializer.errors["email"]:
+                    if hasattr(error, "code") and error.code == "user_exists":
+                        return Response(
+                            status=status.HTTP_409_CONFLICT,
+                            data={"email": ["A user with this email already exists"]},
+                        )
+            return Response(status=status.HTTP_400_BAD_REQUEST, data=serializer.errors)
+
+        user = serializer.save()
+        token = RefreshToken.for_user(user)
+        res = RegisterResponseSerializer(
+            {
+                "user": user,
+                "tokens": {
+                    "refresh": str(token),
+                    "access": str(token.access_token),
+                },
+            }
+        )
+        return Response(status=status.HTTP_201_CREATED, data=res.data)
--- app/backend/users/views.py
+++ app/backend/users/views.py
@@ -2,59 +2,14 @@
 API Views for Users
 """
 
-import logging
 from core.models import Profile
 from core.permissions import UserIsOwner, UserIsOwnerOrReadOnly
 from core.serializers import ProfileSerializer, UserSerializer
 from django.contrib.auth import get_user_model
-from rest_framework import status
-from rest_framework.generics import CreateAPIView, RetrieveUpdateAPIView
-from rest_framework.permissions import AllowAny, IsAuthenticated
-from rest_framework.response import Response
-from rest_framework_simplejwt.tokens import RefreshToken
-
-from .serializers import RegisterRequestSerializer, RegisterResponseSerializer
+from rest_framework.generics import RetrieveUpdateAPIView
+from rest_framework.permissions import IsAuthenticated
 
 User = get_user_model()
-
-
-class UserRegisterView(CreateAPIView):
-    """
-    API View for Registering Users
-    POST - users/regsiter
-    """
-
-    serializer_class = RegisterRequestSerializer
-    permission_classes = [AllowAny]
-
-    def create(self, request):
-        """
-        create function overrides POST logic
-        """
-        serializer = self.get_serializer(data=request.data)
-        if not serializer.is_valid():
-            # Check if the error is due to an existing user
-            if "email" in serializer.errors:
-                for error in serializer.errors["email"]:
-                    if hasattr(error, "code") and error.code == "user_exists":
-                        return Response(
-                            status=status.HTTP_409_CONFLICT,
-                            data={"email": ["A user with this email already exists"]},
-                        )
-            return Response(status=status.HTTP_400_BAD_REQUEST, data=serializer.errors)
-
-        user = serializer.save()
-        token = RefreshToken.for_user(user)
-        res = RegisterResponseSerializer(
-            {
-                "user": user,
-                "tokens": {
-                    "refresh": str(token),
-                    "access": str(token.access_token),
-                },
-            }
-        )
-        return Response(status=status.HTTP_201_CREATED, data=res.data)
 
 
 class UserDetailViewSet(RetrieveUpdateAPIView):
